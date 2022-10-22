e5df4dfcf6107e28bbcc7f2ced2dd3cb556aeaef
UsamaNaveed9 <usamanaveed9263@gmail.com>
2022-10-22 10:00:11 +0000

    fect:cost planning doctype flow completed

--- alamre_cost/alamre_cost/doctype/cost_planning/cost_planning.js
+++ alamre_cost/alamre_cost/doctype/cost_planning/cost_planning.js
@@ -4,5 +4,60 @@
 frappe.ui.form.on('Cost Planning', {
 	// refresh: function(frm) {
 
-	// }
+	// },
+});
+
+frappe.ui.form.on("CP Material Costing", "qty", function(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "total", d.qty * d.unit_price);
+
+	var total = 0;
+		frm.doc.material_costing.forEach(function(d) { total += d.total; });
+		frm.set_value('total_material_cost', total);
+});
+
+frappe.ui.form.on('CP Material Costing', {
+	material_costing_remove:function(frm, cdt, cdn) {
+        var total = 0;
+		frm.doc.material_costing.forEach(function(d) { total += d.total; });
+		frm.set_value('total_material_cost', total);
+	}
+});
+
+frappe.ui.form.on("CP Material Costing", "unit_price", function(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "total", d.qty * d.unit_price);
+
+	var total = 0;
+		frm.doc.material_costing.forEach(function(d) { total += d.total; });
+		frm.set_value('total_material_cost', total);
+});
+
+frappe.ui.form.on("CP Labour Costing", "qty", function(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "total", d.qty * d.unit_price);
+
+	var total = 0;
+		frm.doc.labour_costing.forEach(function(d) { total += d.total; });
+		frm.set_value('total_labour_cost', total);
+		frm.set_value('total_cost', total + frm.doc.total_material_cost);
+});
+
+frappe.ui.form.on("CP Labour Costing", "unit_price", function(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "total", d.qty * d.unit_price);
+
+	var total = 0;
+		frm.doc.labour_costing.forEach(function(d) { total += d.total; });
+		frm.set_value('total_labour_cost', total);
+		frm.set_value('total_cost', total + frm.doc.total_material_cost);
+});
+
+frappe.ui.form.on('CP Labour Costing', {
+	labour_costing_remove:function(frm, cdt, cdn) {
+        var total = 0;
+		frm.doc.labour_costing.forEach(function(d) { total += d.total; });
+		frm.set_value('total_labour_cost', total);
+		frm.set_value('total_cost', total + frm.doc.total_material_cost);
+	}
 });
