74d064c12fe7165fdd652732d38d01cc724c0197
UsamaNaveed9 <usamanaveed9263@gmail.com>
2022-10-22 14:35:57 +0000

    fect: Add custom button in sales order to create cost planning

--- alamre_cost/customjs/sales_order.js
+++ alamre_cost/customjs/sales_order.js
@@ -1,38 +1,15 @@
 
-
 frappe.ui.form.on("Sales Order", {
-    setup: function(frm) {
-		frm.custom_make_buttons = {
-			'Cost Planning': 'Cost Planning'
-		}
-    }
-});
-
-erpnext.selling.SalesOrderController = erpnext.selling.SellingController.extend({
-	refresh: function(doc, dt, dn) {
-		var me = this;
-		this._super();
-
-		if (doc.docstatus==1) {
-
-			
-			if(doc.status !== 'Closed') {
-				if(doc.status !== 'On Hold') {
-					
-					// Cost Planning
-					if(flt(doc.per_billed, 6) < 100) {
-						this.frm.add_custom_button(__('Cost Planning'), () => me.make_cost_planning(), __('Create'));
-					}
-				}
-			}
+    refresh: function(frm) {
+		if(frm.doc.docstatus === 1 && frm.doc.status !== 'Closed') {
+			frm.add_custom_button(__('Create Cost Planning'), () => {
+				frappe.model.open_mapped_doc({
+					method: "alamre_cost.custompy.sales_order.make_cost_planning",
+					frm: cur_frm
+				})
+			});
 		}
 	},
-
-	make_cost_planning: function() {
-		frappe.model.open_mapped_doc({
-			method: "alamre_cost.custompy.sales_order.make_cost_planning",
-			frm: this.frm
-		})
-	},
-
 });
+
+
